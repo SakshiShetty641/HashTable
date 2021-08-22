edb93cb635e60c7fea073a0ff0c43773e2546ae8
Sakshi <sakshishetty1231997@gmail.com>
2021-08-22 17:23:22 +0530

    Added: Method to find frequency of words in a sentence

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,12 +1,29 @@
 package com.bridgelabz;
 /**
- * Purpose - To perform Hashtable Operations
+ * Purpose - Ability to find frequency of words in a sentence - “To be or not to be”
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-22
  */
 public class HashTableMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to the program");
+        MyHashTable<String, Integer> hashTable = new MyHashTable();
+        String sentence = "To be or not to be";
+
+        // Storing given sentence in an array.
+        // Converting the sentence to lower case and removing spaces.
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+        // Iterating over the array.
+        for (String word : sentenceArray) {
+            Integer count = hashTable.get(word);
+
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashTable.add(word, count);
+        }
+        System.out.println(hashTable);
     }
 }
