31023f56b3fae9a23470a6421b91c96a32c9695a
Sakshi <sakshishetty1231997@gmail.com>
2021-08-22 16:55:14 +0530

    Added: Welcome Message to the Program

---
+++ src/com/bridgelabz/HashTableMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To perform Hashtable Operations
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-22
+ */
+public class HashTableMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program");
+    }
+}
